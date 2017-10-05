d963d4e5cb0d66e4aa35e2c1932db1eaca33b849
Wataru Ikeda Kay <watarukay@Watarus-MacBook-Pro.local>
2017-10-05 18:04:36 +0900

    Fix gh-page routing issue

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Route path='/' component={App} />
   </BrowserRouter>,
   document.getElementById("root")
